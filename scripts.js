b010a04b3640a7c49f43f75e371d459c8cd3b860
Shane Graff <copellius@gmail.com>
2022-08-12 11:28:42 -0700

    Fix type in scripts.js that was causing the canvas to not show

--- scripts.js
+++ scripts.js
@@ -1,16 +1,17 @@
-const canvas = document.getElementById("canvas");
+const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 const CANVAS_WIDTH = canvas.width = 600;
 const CANVAS_HEIGHT = canvas.height = 600;
 
 const playerImage = new Image();
-playerImage.src = './assets/shadow_dog.png';
+playerImage.src = 'assets/shadow_dog.png';
 
 function animate(){
   ctx.clearRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
-  ctx.fillRect(50,50,100,100);
+  ctx.fillRect(100,50,100,100);
+  ctx.drawImage(playerImage, 0, 0);
   requestAnimationFrame(animate);
 };
 animate();
 
-//time 7:24
+//time 7:32
